16f2e3c350abcba6f1c5e66938dfbd31f311aaa8
Apoorv Kumar <axk132231@utdallas.edu>
2016-03-14 13:51:23 -0500

    First commit
    Change-Id: I3f19f0e86058c3c03be643c100ffc250f7fc915b

---
+++ UniqueString/src/uniqueIdentifier.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class uniqueIdentifier {
+
+	static boolean checkUniqueString(String arr){
+		if (arr == null ){
+			System.out.println("Enter a valid String");
+			return false;
+		}
+
+		if (arr.length()==1){
+			System.out.println("Unique number");
+			return true;
+		}
+
+		int len = arr.length();
+		Set<Character> set = new HashSet<Character>();
+		for (int i=0;i<len;++i){
+			set.add(arr.charAt(i));
+		}
+		if (set.size()==arr.length()){
+			return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		String arr ="Apoorv";
+		if (checkUniqueString(arr)){
+			System.out.println("Unique");
+		}else {
+			System.out.println("Not Unique");
+		}
+	}
+
+}
