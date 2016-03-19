0c64e8e6d97eb7a3134fb3d46566d105f3d432d1
Apoorv Kumar <axk132231@utdallas.edu>
2016-03-18 21:53:50 -0500

    Attempt 2 :/
    Change-Id: I441aa9a4bf6f47b9d5456ba404e763dc436a3e6a

--- OneAway/src/OneEditAway.java
+++ OneAway/src/OneEditAway.java
@@ -1,7 +1,7 @@
 import javax.annotation.processing.SupportedSourceVersion;
 
 public class OneEditAway {
-	
+
 	public static String sort(String str){
 		char[] a=str.toCharArray();
 		java.util.Arrays.sort(a);
@@ -9,73 +9,65 @@ public class OneEditAway {
 	}
 
 	public static boolean insert(String str1,String str2){
-		
-		
+
+
 		return true;
 	}
-	
-	public static void remove(String str1,String str2){
-		
-		sort(str1);
-		sort (str2);
-		int c = 0,ik=0;
-		int[] letters = new int[128];
-		int[] letters1 = new int[128];
 
-		
-		char[] char1 = str1.toCharArray();
-		char[] char2 = str2.toCharArray();
-		for (char c1: char2){
-			letters[c1]++;
+	public static void remove(String str1,String str2){
+		int count=0;
+		int[] letters = new int[128];
+		char[] toChar1 = str1.toCharArray();
+		char[] toChar2 = str2.toCharArray();
+
+		//so i have counted the number of each character
+		for (char c: toChar1){
+			letters[c]++;
 		}
-		for (char c2: char1){
-			letters1[c2]++;
-		}
-		
+		//2 scan approach: says scan once to count and scan second time to counter check
 		if (str1.length()==str2.length()+1){
 			for (int i=0;i<str2.length();i++){
-				for(int j=0;j<str1.length();j++){
-					System.out.println("Comparing "+str2.charAt(i)+" and "+str1.charAt(j));
+				int c=str2.charAt(i);
+				letters[c]--;
+				if (letters[c]==0){
+					count = count+1;
+					System.out.println(count);
 					
-					if (str2.toCharArray()[i]==str1.toCharArray()[j]){
-						c=c+1;
-						break;
-					}else{
-						ik=ik+1;
-//						System.out.println(ik);
-					}
 				}
-			}
-			
-			if (c==str2.length()){
-				System.out.println(str1+" , "+str2+" ---> "+"True");
 				
-			}else{
-				System.out.println(str1+" , "+str2+" ---> "+"False");
 			}
-		}else{System.out.println("Sorry extra changes");}
-		
-		
+			if (count == str2.length()){
+				System.out.println(str1+" , "+str2+" ---> "+"true");
+			}else{
+				System.out.println(str1+" , "+str2+" ---> "+"false");
+			}
+
+
+
+
+
+
+		}
 	}
-	
+
 	public static boolean replace(String str1,String str2){
-		
-		
+
+
 		return true;
-		
+
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str1 ="master";
-        String str2 ="sstte";
-        
-//        if (insert(str1,str2) || remove(str1,str2) || replace(str1,str2)){
-//        	System.out.println(str1+" , "+str2+" ---> "+"true");
-//        }else{
-//        	System.out.println(str1+" , "+str2+" ---> "+"false");
-//        }
-        
-        remove(str1,str2);
+		String str1 ="Apoorv";
+		String str2 ="vroop";
+
+		//        if (insert(str1,str2) || remove(str1,str2) || replace(str1,str2)){
+		//        	System.out.println(str1+" , "+str2+" ---> "+"true");
+		//        }else{
+		//        	System.out.println(str1+" , "+str2+" ---> "+"false");
+		//        }
+
+		remove(str1,str2);
 	}
 
 }
