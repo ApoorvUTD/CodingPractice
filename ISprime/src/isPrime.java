de1d5898fb2d9701c5178d8616c7fc676bafbd21
Apoorv Kumar <axk132231@utdallas.edu>
2016-03-21 10:54:24 -0500

    Using primeSieve assume every number is prime than depending on condition mark it out Change-Id: I960c55a0f6c3f12818460b4b3d3ea3c067ea8691

---
+++ ISprime/src/isPrime.java
@@ -0,0 +1,83 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class isPrime {
+	
+	static boolean checkPrime(int n){
+		for (int i = 2;i*2<n;i++){
+			System.out.println("Checking divisibility by -->"+i);
+			if ((n%i)==0){
+				System.out.println("Trying"+ i);
+				return false;
+			}
+		}
+		return true;
+	}
+	
+//	IsPrime: check just from 2 wether its divisibly by 2 or not(not efficient as you have to go all the way just 
+//			to gurantee prime, you can also check till n/2 or best go till I*I as this also solves are problem kind of 
+//			mathematical so "keep calm and code")oh okay so there is another technique known as Prime Sieve(very similar to 
+//					finding unique but with a pinch of lemon I.e. suppose all numbers are prime then pick 2 mark out its multiple,
+//					done pick 3 mark out its multiple done and so on.)
+	
+	static void primeSieve(int n){
+		boolean[] primesieve = new boolean[n];
+		Arrays.fill(primesieve,true);
+		primesieve[0]=primesieve[1]=false;
+		for (int i=2;i<primesieve.length;i++){
+			if(primesieve[i]){
+			for (int j=2;j*i<primesieve.length;j++){
+				primesieve[j*i]=false;
+			}
+			}
+			
+		}
+	}
+
+	//returns the count of prime number which are there till n
+	 public static int countPrimes(int n) {
+		 int c =0;
+	        boolean[] primecount = new boolean[n];
+	        Arrays.fill(primecount,true);
+	        if (n==0){
+	        primecount[0]=false;
+	        }
+	        
+	        if (n==1){
+	        primecount[0]=false;	
+	        primecount[1]=false;
+	        }
+	        
+	        if(n>1){
+	        	primecount[0]=false;	
+		        primecount[1]=false;
+	        for(int i=2;i<primecount.length;i++){
+	            if (primecount[i]){
+	                for (int j=2;j*i<primecount.length;j++){
+	                    primecount[j*i]=false;
+	                }
+	            }
+	        }
+	        }
+	        
+	        for(int i=0;i<primecount.length;i++){
+	        	if (primecount[i]){
+	        		c=c+1;
+	        	}
+	        }
+	        
+	        return c;
+	        
+	    }
+	    
+
+	public static void main(String[] args) {
+//		if(checkPrime(489)){
+//			System.out.println("Yes, Sir its prime");
+//		}else{
+//			System.out.println("Sorry its not prime");
+//		}
+		
+		System.out.println(countPrimes(10));
+	}
+}
