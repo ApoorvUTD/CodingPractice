76ad8dc4d4c484d40d0006c7f8ee020d81c0cd9a
Apoorv Kumar <axk132231@utdallas.edu>
2016-03-21 15:22:22 -0500

    2 pointer approach
    Change-Id: I5b0873070cba41ad1606ebe766684b1641c03562

---
+++ TwoSumII/src/TWOsUMsORTED.java
@@ -0,0 +1,37 @@
+//Similar to Question [1. Two Sum], except that the input array is already sorted in
+//ascending order.
+public class TWOsUMsORTED {
+	
+	
+	//binary search approach
+	public static void twoSum(int[] array,int target){
+		
+	}
+	
+	//two pointer approach
+	public static void twoSum2pointer(int[] array,int target){
+		
+			int j=array.length-1;
+			int i=0;
+			while(i<j){
+			if(array[i]+array[j]>target){
+				j--;
+			}
+			if(array[i]+array[j]<target){
+				i++;
+			}
+			else{
+				System.out.println(array[i]+array[j]);
+				break;
+			}
+		}
+	
+	}
+	
+	public static void main(String[] args) {
+		int[] array={2,4,3,1,6,7,8};
+		int target=15;
+		twoSum2pointer(array,target);
+	}
+
+}
