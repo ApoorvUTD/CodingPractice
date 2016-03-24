713fdd9009b912de098a8f154e6d92a173534d47
Apoorv Kumar <axk132231@utdallas.edu>
2016-03-24 10:43:35 -0500

    validPalindrome check
    Change-Id: I20ed543925c5973439dce95cf7b3419c75d7da2c

---
+++ ValidPalindrome/src/validPalindrome.java
@@ -0,0 +1,37 @@
+
+public class validPalindrome {
+
+	public static void checkPalindrome(String str){
+		String clean= str.replaceAll("[^a-zA-Z]+","").toLowerCase();
+		System.out.println(clean);
+		char[] charArray = clean.toCharArray();
+		System.out.println(charArray.length);
+		int len = 1;
+		for (int i=0;i<charArray.length-1;i++){
+
+			if (charArray[i]==charArray[charArray.length-1-i]){
+				len=len+1;
+			}else{
+				len = len -1;
+			}
+		}
+		
+		if (len==charArray.length){
+			System.out.println("Yup it is ");
+		}else{
+			System.out.println("cmon man give me something");
+		}
+	}
+
+
+
+	public static void main(String[] args) {
+
+		String str1 ="A man, a plan, a canal: Panama";
+		String str2="race a car";
+		System.out.println(str1);
+		checkPalindrome(str1);
+		checkPalindrome(str2);
+
+	}
+}
