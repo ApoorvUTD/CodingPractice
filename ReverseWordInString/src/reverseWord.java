7f965aebabc77fc3fd90535bee6bee2f698c8f2c
Apoorv Kumar <axk132231@utdallas.edu>
2016-03-28 21:07:37 -0500

    Change-Id: If779b164a84212636ef3b921ceeab339bfbf05ac

---
+++ ReverseWordInString/src/reverseWord.java
@@ -0,0 +1,30 @@
+
+public class reverseWord {
+
+	public static String reverseIt(String str){
+		StringBuilder reverse = new StringBuilder();
+		int j = str.length();
+		
+		for(int i=str.length()-1;i>=0;i--){
+			if(str.charAt(i)==' '){
+				j=i;
+			}else if(i==0 || str.charAt(i-1)==' '){
+				if(reverse.length()!=0){
+					reverse.append(' ');
+				}
+				reverse.append(str.substring(i, j));
+			}
+		}
+
+
+		return reverse.toString();
+	}
+
+
+
+	public static void main(String[] args) {
+		System.out.println(reverseIt("My name is Apoorv"));
+
+	}
+
+}
